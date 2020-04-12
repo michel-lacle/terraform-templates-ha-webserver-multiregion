5e6d7365896a42faf247955444cb50c775b9fa7c
papiamento <1024836+papiamento@users.noreply.github.com>
2020-04-12 15:44:31 -0400

    add latency routing policy

---
+++ aws_route53_latency.tf
@@ -0,0 +1,27 @@
+resource "aws_route53_record" "w-north-america" {
+  zone_id = data.aws_route53_zone.f1kart-zone.zone_id
+  name    = "weighted-ha"
+  type    = "A"
+  ttl     = "5"
+
+  weighted_routing_policy {
+    weight = 50
+  }
+
+  set_identifier = "north-america"
+  records        = [module.ha_webserver_usa.ec2-ip]
+}
+
+resource "aws_route53_record" "w-ha-asia" {
+  zone_id = data.aws_route53_zone.f1kart-zone.zone_id
+  name    = "weighted-ha"
+  type    = "A"
+  ttl     = "5"
+
+  weighted_routing_policy {
+    weight = 50
+  }
+
+  set_identifier = "asia"
+  records        = [module.ha_webserver_asia.ec2-ip]
+}
