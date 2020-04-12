90ef33c049270519b9150bfd3e0284bbdb9ce3fb
papiamento <1024836+papiamento@users.noreply.github.com>
2020-04-12 15:45:29 -0400

    add latency routing policy

--- aws_route53_latency.tf
+++ aws_route53_latency.tf
@@ -1,25 +1,25 @@
-resource "aws_route53_record" "w-north-america" {
+resource "aws_route53_record" "l-north-america" {
   zone_id = data.aws_route53_zone.f1kart-zone.zone_id
-  name    = "weighted-ha"
+  name    = "latency-ha"
   type    = "A"
   ttl     = "5"
 
-  weighted_routing_policy {
-    weight = 50
+  latency_routing_policy {
+    region = var.regions.north-america
   }
 
   set_identifier = "north-america"
   records        = [module.ha_webserver_usa.ec2-ip]
 }
 
-resource "aws_route53_record" "w-ha-asia" {
+resource "aws_route53_record" "l-ha-asia" {
   zone_id = data.aws_route53_zone.f1kart-zone.zone_id
-  name    = "weighted-ha"
+  name    = "latency-ha"
   type    = "A"
   ttl     = "5"
 
-  weighted_routing_policy {
-    weight = 50
+  latency_routing_policy {
+    region = var.regions.asia
   }
 
   set_identifier = "asia"
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "regions" {
+  type = "map"
+
+  default = {
+    north-america = "us-east-1"
+    asia = "ap-northeast-2"
+  }
+}
