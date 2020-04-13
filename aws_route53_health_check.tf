cd9a38c49a16c4214b684ea989d507fdae20d9e1
papiamento <1024836+papiamento@users.noreply.github.com>
2020-04-13 07:38:07 -0400

    adding asia health check

--- aws_route53_health_check.tf
+++ aws_route53_health_check.tf
@@ -10,3 +10,16 @@ resource "aws_route53_health_check" "north-america-health-check" {
     Name = "north-america-health-check"
   }
 }
+
+resource "aws_route53_health_check" "asia-health-check" {
+  ip_address = module.ha_webserver_asia
+  port              = 80
+  type              = "HTTP"
+  resource_path     = "/"
+  failure_threshold = "5"
+  request_interval  = "30"
+
+  tags = {
+    Name = "asia-health-check"
+  }
+}
